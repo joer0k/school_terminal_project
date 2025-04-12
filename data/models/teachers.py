465181721bcf7c508fe3896b996e7b0e1d7fd38f
Anzhelica <angelica.sobol@gmail.com>
2025-04-12 18:46:47 +0300

    Создала таблицу должностей

--- data/models/teachers.py
+++ data/models/teachers.py
@@ -1,5 +1,6 @@
 import sqlalchemy
 from sqlalchemy import orm
+from sqlalchemy.orm import relationship
 
 from data.db_session import SqlAlchemyBase
 
@@ -9,5 +10,19 @@ class Teachers(SqlAlchemyBase):
 
     id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
     teacher_name = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    post_id = sqlalchemy.Column(sqlalchemy.ForeignKey("posts.id"))
+    way_to_photo = sqlalchemy.Column(sqlalchemy.String, unique=True)
+    additional_information = sqlalchemy.Column(sqlalchemy.Text)
 
+    posts = relationship('Posts')
     t_t_s = orm.relationship('Teachers_to_Subjects', back_populates='teacher')
+
+
+class Posts(SqlAlchemyBase):
+    __tablename__ = "posts"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False, unique=True)
+
+    teachers = orm.relationship("Teachers", back_populates='posts')
+
